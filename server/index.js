63abe4ccb5be7e3fa6d4767fd2818d78d62d5436
aayushk9 <ayushkokate1@gmail.com>
2024-02-16 15:38:54 +0530

    database added

--- server/index.js
+++ server/index.js
@@ -1,13 +1,45 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
+const URI = process.env.MONGODB_URI
 const port = 4000
 
-app.get('/', (req, res) => {
-  res.send('Hello from backend');
+mongoose.connect(URI)
+const database = mongoose.connection;
+
+database.on('err', (err)=> {
+  console.log(err);
 });
 
+database.once('connected', ()=> {
+  console.log('Database connected to the server'); 
+});
+
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, '../client/pages/home.html');
+  // rendering home page
+  res.sendFile(filePath);
+}); 
+
+app.post('/', (req, res) => {
+
+})   
+
+app.get('/blog', (req, res) => {
+  const filePath = path.join(__dirname, '../client/pages/blog.html');
+  res.sendFile(filePath);    
+}); 
+
+app.get('/open-source', (req, res) => {
+  const filePath = path.join(__dirname, '../client/pages/open-source.html');
+  res.sendFile(filePath);
+}) 
+ 
 app.listen(port, (req, res) => {  
     console.log(`Server is listening on port ${port}`);
 });
 
+
+// hashnode api => d159f53d-4dec-4993-b167-b8b80f8db555
